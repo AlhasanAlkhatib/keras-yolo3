6ae0b9957e4adaf65c5bc9e82920eb979fe1db91
Alhasan Alkhatib <37053503+AlhasanAlkhatib@users.noreply.github.com>
2019-03-24 16:53:05 +0300

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -23,7 +23,7 @@ class YOLO(object):
         #"model_path": 'model_data/yolo.h5',
         "model_path": 'logs/000/trained_weights_stage_1.h5',
         "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/coco_classes.txt',
+        "classes_path": 'model_data/voc_classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
